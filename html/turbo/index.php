8c18b7516336895e40d85cfff6a54fa459147a7b
crazyyy <crazyyy@gmail.com>
2015-07-10 12:40:38 +0300

    before turbo

---
+++ html/turbo/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="x-rim-auto-match" content="none">
+    <meta name="language" content="ru" />
+    <meta name="description" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/theme.css">
+    <link rel="stylesheet" type="text/css" href="css/fancySelect.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/fancySelect.js"></script>
+    <script src="js/need.js"></script>
+    <title>Узнай наличие и цену запчасти</title>
+    <script type="text/javascript">
+        /* init Call Service */
+        var CallSiteId = 'f8d62754464a50f84a66e68cdf6e9370';
+        var CallBaseUrl = '//uptocall.com';
+        (function() {
+            var lt = document.createElement('script');
+            lt.type ='text/javascript';
+            lt.charset = 'utf-8';
+            lt.async = true;
+            lt.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + CallBaseUrl + '/widget/client.js';
+            var sc = document.getElementsByTagName('script')[0];
+            if (sc) sc.parentNode.insertBefore(lt, sc);
+            else document.documentElement.firstChild.appendChild(lt);
+        })();
+    </script>
+
+</head>
+<body>
+<div class="modal">
+    <a class="close">закрыть</a>
+    <div class="clear"></div>
+    <h1>Ваша заявка отправлена</h1>
+    <h3>Наш консультант ответит вам в течение 20 мин</h3>
+</div>
+<div id="header">
+    <div class="center">
+
+        <div class="logo">
+            <img src="./images/logo.png" alt="Rrecamgroup.ru" title="Rrecamgroup.ru">
+            <label class="drop">/</label>
+            <label class="phone">
+                8 499 394 49 80<br>
+                8 925 765 06 56<br>
+                recamgr@gmail.com
+            </label>
+        </div>
+        <div class="slogan arrow_box">
+            Запчасти для спецтехники и грузовиков
+        </div>
+    </div>
+</div>
+<div id="main_body">
+    <div class="center">
+        <h1 class="main_title">Узнай наличие и цену запчасти</h1>
+        <div id="control">
+        <ul class="main_ul">
+            <!--<li style="width: 186px">
+                <div id="div_clent_hodovoy">
+                <select name="client_hodovoy" id="client_hodovoy" onchange="update()" class="form-control">
+                    <option value="0">Выберите тип детали</option>
+                    <option>Ходовая часть</option>
+                    <option>Запчасти двигателя</option>
+                    <option>Навесное оборудование</option>
+                    <option>Гидравлика</option>
+                    <option>Запчасти для ТО</option>
+                </select>
+                </div>
+                <input type="hidden" id="input_client_hodovoy" name="input_client_hodovoy" value="0">
+            </li>-->
+            <li style="width: 432px"><input type="text" class="main_input" name="client_comment" id="client_comment"   placeholder="введите артикул либо краткое описание запчасти"></li>
+            <li style="width: 160px"><input type="text" class="main_input" name="client_phone" id="client_phone"  placeholder="телефон"></li>
+            <li style="width: 186px">
+                <input type="text" class="main_input" name="client_email" id="client_email"  placeholder="email">
+            </li>
+            <li style="width: 184px"><a href="javascript:void(0)" class="main_button">Отправить</a></li>
+        </ul>
+        </div>
+    </div>
+</div>
+<div id="efir">
+    <div class="center">
+        <h2>Онлайн эфир запросов о наличии и цене посетителей сайта</h2>
+
+        <div class="efir-block">
+            <ul class="efir_ul_head">
+                <li class="width105">время запроса</li>
+                <li class="width480">результат</li>
+                <li class="width153">средняя цена  на рынке</li>
+                <li class="width139">наша цена для клиента</li>
+            </ul>
+            <div class="clear"></div>
+            <div class="efir-line"></div>
+            <div class="overflow">
+                <ul id="efir_dinamic">
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div id="static">
+    <div class="center">
+        <h4>Самый большой склад запчастей для<br>
+            дорожно-строительной техники</h4>
+        <div class="static_count">
+            <label>4 000 000</label>
+            <div class="static_lenta">запчастей на складе</div>
+        </div>
+    </div>
+</div>
+<div id="how">
+    <div class="center">
+        <h2>Как происходит покупка у нас</h2>
+        <ul class="how_ul">
+            <li style="width:315px">
+                <div class="round">1</div>
+                <label class="how_label">
+                    заказ по телефону<br>
+                    или через форму
+                </label>
+            </li>
+            <li style="width: 330px;">
+                <div class="round">2</div>
+                <label class="how_label" style="margin: 20px 0 0 35px;">
+                    оплата
+                </label>
+            </li>
+            <li>
+                <div class="round">3</div>
+                <label class="how_label" style="margin: 15px 0 0 33px;">
+                    поставка товара<br>
+                     <span class="how_span">
+                    (все регионы РФ)
+                </span>
+                </label>
+            </li>
+        </ul>
+        <div class="clear"></div>
+        <div class="line"></div>
+        <div class="clear"></div>
+        <div class="border"></div>
+        <div class="clear"></div>
+        <h4>1-3 дня</h4>
+    </div>
+</div>
+<!--
+<div id="chat">
+    <div class="center">
+        <div class="chat_widget">
+            <h1 class="chat_widget_title">Онлайн консультант</h1>
+            <div class="chat_widget_status">
+                <div class="color yellow"></div>
+                в сети
+            </div>
+
+            <div class="chat_widget_tablo">
+                <label class="chat_widget_line">Оператор на линии</label>
+                <ul class="chat_widget_ul">
+                    <li class="chat_widget_out"><span class="chat_widget_time">15:55</span> <i>Здравствуйте чем я могу вам помочь</i></li>
+                    <li class="chat_widget_in"></li>
+                </ul>
+            </div>
+            <div class="chat_widget_form">
+                <input class="chat_widget_input" name="chat_widget_input" id="chat_widget_input" placeholder="введите ваш вопрос">
+                <a href="javascript:void(0)" class="chat_widget_button">Отправить</a>
+            </div>
+            <img src="./images/manager.png" class="chat_widget_manger">
+        </div>
+    </div>
+</div>-->
+
+<div id="brend">
+    <div class="center">
+        <h4>Бренды с которыми мы работаем</h4>
+        <img src="./images/brand.gif">
+    </div>
+</div>
+
+<div id="order">
+    <div class="center">
+        <h3>Закажи запчасти по телефону</h3>
+        <h2>8 499 394 49 80</h2>
+    </div>
+</div>
+
+<div id="client">
+    <div class="center">
+        <h4>Наши клиенты</h4>
+        <img style="width: 930px" src="./images/clients.gif">
+    </div>
+</div>
+<div class="clear"></div>
+<div id="footer">
+    <div class="center">
+        <label><?php echo date('Y');?> &copy; Recam Group </label>
+        <label style="float: right">Email: <a href="mailto:recamgr@gmail.com" >recamgr@gmail.com</a></label>
+
+    </div>
+</div>
+<!--
+<link rel="stylesheet" href="//cdn.callbackhunter.com/widget/tracker.css">
+<script type="text/javascript"
+        src="//cdn.callbackhunter.com/widget/tracker.js" charset="UTF-8"></script >
+<script type="text/javascript">var hunter_code="1eb7e7d65d17ba13c9e93255d79d1361";</script>
+-->
+<link rel="stylesheet" href="//cdn.callbackhunter.com/widget/tracker.css">
+<script type="text/javascript"
+        src="//cdn.callbackhunter.com/widget/tracker.js" charset="UTF-8"></script >
+<script type="text/javascript">var hunter_code="7ed9f6bcf5fbcb43319042e3bd909271";</script>
+
+</body>
+</html>
